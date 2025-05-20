b84b4e43ed0aa2c04a20b1d66e334fcebf9e0d8e
RatzzFatzz <michael@ratzloeffel.de>
2025-05-20 02:45:14 +0200

    Implement anilist api access via graphql

---
+++ src/main/java/at/pcgamingfreaks/model/anilist/AnilistMediaTitle.java
@@ -0,0 +1,11 @@
+package at.pcgamingfreaks.model.anilist;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AnilistMediaTitle {
+    private String romaji;
+    private String english;
+}
