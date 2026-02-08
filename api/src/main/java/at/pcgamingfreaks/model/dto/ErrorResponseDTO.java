169eff11adeb077fdd8775bc4594fdc45334b45e
RatzzFatzz <github@ratzloeffel.de>
2026-02-08 20:10:08 +0100

    Fix global exception handling

--- api/src/main/java/at/pcgamingfreaks/model/dto/ErrorResponseDTO.java
+++ api/src/main/java/at/pcgamingfreaks/model/dto/ErrorResponseDTO.java
@@ -1,7 +1,11 @@
 package at.pcgamingfreaks.model.dto;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @RequiredArgsConstructor
 public class ErrorResponseDTO {
     private final String error;
