a4e1eb2b21a8463a66cfde9060c5d52e05f65918
RatzzFatzz <github@ratzloeffel.de>
2026-02-14 11:10:48 +0100

    Remove unused domain objects

--- api/src/main/java/at/pcgamingfreaks/model/dto/ChangePasswordResponseDTO.java
+++
@@ -1,8 +0,0 @@
-package at.pcgamingfreaks.model.dto;
-
-public class ChangePasswordResponseDTO extends GenericResponseDTO {
-    public ChangePasswordResponseDTO(boolean success, String message) {
-        this.success = success;
-        this.message = message;
-    }
-}
