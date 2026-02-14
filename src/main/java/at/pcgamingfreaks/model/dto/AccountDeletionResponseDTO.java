3db85a087992cf9c1897a3814af54cee7d0d8776
RatzzFatzz <github@ratzloeffel.de>
2026-02-14 11:10:48 +0100

    Remove unused domain objects

--- src/main/java/at/pcgamingfreaks/model/dto/AccountDeletionResponseDTO.java
+++
@@ -1,8 +0,0 @@
-package at.pcgamingfreaks.model.dto;
-
-public class AccountDeletionResponseDTO extends GenericResponseDTO {
-    public AccountDeletionResponseDTO(boolean success, String message) {
-        this.success = success;
-        this.message = message;
-    }
-}
