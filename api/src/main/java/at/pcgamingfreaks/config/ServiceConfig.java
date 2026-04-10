90b998bf742581e3fec5c438b95740a227e8f06f
RatzzFatzz <github@ratzloeffel.de>
2026-04-10 10:25:32 +0200

    Fix property loading for third-party service configs

--- api/src/main/java/at/pcgamingfreaks/config/ServiceConfig.java
+++ api/src/main/java/at/pcgamingfreaks/config/ServiceConfig.java
@@ -2,16 +2,13 @@ package at.pcgamingfreaks.config;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 
 import static io.micrometer.common.util.StringUtils.isNotBlank;
 
 @Getter
 @Setter
 public class ServiceConfig implements ThirdPartyServiceConfig{
-	@Value("${client.key}")
 	private String key;
-	@Value("${client.secret}")
 	private String secret;
 	private String redirectUrl;
 
