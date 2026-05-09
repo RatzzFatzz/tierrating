61d2652d92ee37285e78919a775b69ed20829d48
RatzzFatzz <github@ratzloeffel.de>
2026-05-09 18:18:45 +0200

    Fix node url rewrites

--- web/next.config.ts
+++ web/next.config.ts
@@ -2,18 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	images: {
-		remotePatterns: [new URL("https://s4.anilist.co/file/anilistcdn/media/**/*.*"), new URL("https://image.tmdb.org/t/p/**/*.*")],
+		remotePatterns: [
+			new URL("https://s4.anilist.co/file/anilistcdn/media/**/*.*"),
+			new URL("https://image.tmdb.org/t/p/**/*.*")
+		],
 	},
 	reactStrictMode: false,
-	output: "standalone",
-	async rewrites() {
-		return [
-			{
-				source: "/api/:path*",
-				destination: `${process.env.API_URL || "http://localhost:8080"}/api/:path*`,
-			},
-		];
-	},
+	output: "standalone"
 };
 
 export default nextConfig;
