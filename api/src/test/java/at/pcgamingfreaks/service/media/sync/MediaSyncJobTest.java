98c4e2f39e331d3f95f090f7d8255bcd91851c29
RatzzFatzz <github@ratzloeffel.de>
2026-08-07 23:15:47 +0200

    Add test for MediaSyncJob

---
+++ api/src/test/java/at/pcgamingfreaks/service/media/sync/MediaSyncJobTest.java
@@ -0,0 +1,63 @@
+package at.pcgamingfreaks.service.media.sync;
+
+import at.pcgamingfreaks.model.db.SyncJob;
+import at.pcgamingfreaks.model.db.User;
+import at.pcgamingfreaks.model.enums.MediaSource;
+import at.pcgamingfreaks.model.enums.MediaType;
+import at.pcgamingfreaks.model.enums.SyncStatus;
+import at.pcgamingfreaks.model.repo.SyncJobRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class MediaSyncJobTest {
+
+	@Mock
+	private MediaSyncProcessor mediaSyncProcessor;
+
+	@Mock
+	private SyncJobRepository syncJobRepository;
+
+	@Test
+	void run_successful() {
+		SyncJob syncJob = new SyncJob();
+		syncJob.setId(1L);
+		syncJob.setUser(new User());
+		syncJob.setMediaSource(MediaSource.ANILIST);
+		syncJob.setMediaType(MediaType.ANIME);
+
+		MediaSyncJob underTest = new MediaSyncJob(syncJob, mediaSyncProcessor, syncJobRepository);
+		underTest.run();
+
+		verify(mediaSyncProcessor, times(1)).processSync(any(), any(), any());
+		verify(syncJobRepository, times(1)).updateStatus(anyLong(), any(SyncStatus.class));
+		verify(syncJobRepository, times(1)).completeJob(anyLong(), eq(SyncStatus.COMPLETED), any(LocalDateTime.class));
+		verify(syncJobRepository, times(0)).completeJob(anyLong(), eq(SyncStatus.FAILED), any(LocalDateTime.class));
+	}
+
+	@Test
+	void run_failed() {
+		doThrow(RuntimeException.class).when(mediaSyncProcessor).processSync(any(), any(), any());
+
+		SyncJob syncJob = new SyncJob();
+		syncJob.setId(1L);
+		syncJob.setUser(new User());
+		syncJob.setMediaSource(MediaSource.ANILIST);
+		syncJob.setMediaType(MediaType.ANIME);
+
+		MediaSyncJob underTest = new MediaSyncJob(syncJob, mediaSyncProcessor, syncJobRepository);
+		underTest.run();
+
+		verify(mediaSyncProcessor, times(1)).processSync(any(), any(), any());
+		verify(syncJobRepository, times(1)).updateStatus(anyLong(), any(SyncStatus.class));
+		verify(syncJobRepository, times(0)).completeJob(anyLong(), eq(SyncStatus.COMPLETED), any(LocalDateTime.class));
+		verify(syncJobRepository, times(1)).completeJob(anyLong(), eq(SyncStatus.FAILED), any(LocalDateTime.class));
+	}
+}
